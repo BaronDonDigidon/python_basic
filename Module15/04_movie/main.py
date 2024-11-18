0602ff310e2c16eb6e70ffff054e909e2c6be762
Gevork <meletjan.kevr94@yandex.ru>
2024-11-18 15:57:19 +0300

    module 15 add task 4

--- Module15/04_movie/main.py
+++ Module15/04_movie/main.py
@@ -1,5 +1,19 @@
-films = ['Крепкий орешек', 'Назад в будущее', 'Таксист',
+films: list[str] = ['Крепкий орешек', 'Назад в будущее', 'Таксист',
          'Леон', 'Богемская рапсодия', 'Город грехов',
          'Мементо', 'Отступники', 'Деревня']
+lover_films: list[str] = []
 
-# TODO здесь писать код
+
+def choose_films(film: str):
+    if film in films:
+        return lover_films.append(films[films.index(film)])
+    return print(f"Ошибка фильма {film} у нас нет")
+
+
+
+
+film_cunter: int = int(input("Сколько фильмов хотите добавить: "))
+for _ in range(film_cunter):
+    film_name = input("Введите название фильма: ")
+    choose_films(film_name)
+print(f"Ваш список любимых фильмов: {', '.join(lover_films)}.")
