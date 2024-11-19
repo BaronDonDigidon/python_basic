87137671e9a4abddfe9303a9baa9c620bbf35953
Gevork <meletjan.kevr94@yandex.ru>
2024-11-20 02:14:00 +0300

    add task 6

--- Module15/06_running_nums/main.py
+++ Module15/06_running_nums/main.py
@@ -1,1 +1,12 @@
-
+number_elements: int = int(input("Введите количество элементов списка: "))
+num_list: list[int] = []
+for _ in range(number_elements):
+    num: int
+    num_list.append(num)
+step: int = int(input("Введите шаг сдвига: "))
+new_num_list: int = []
+index: int = -step
+while index != len(num_list) - step:
+    new_num_list.append(num_list[index])
+    index += 1
+print(new_num_list)
