cdf551119e4e133729b33401f1ed7c284da82462
Gevork <meletjan.kevr94@yandex.ru>
2024-11-19 21:41:06 +0300

    add task 3

--- Module15/01_list_gen/main.py
+++ Module15/01_list_gen/main.py
@@ -3,4 +3,3 @@ list_of_odd_numbers: list[int] = []
 for digit in range(1, num + 1, 2):
      list_of_odd_numbers.append(digit)
 print(f"Список из нечётных чисел от одного до N: {list_of_odd_numbers}")
-
