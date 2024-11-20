d1f45418f06cdf4d0bf427523f091976763bebb2
Gevork <meletjan.kevr94@yandex.ru>
2024-11-20 13:22:39 +0300

    Module 15 task 6 remake

--- Module15/06_running_nums/main.py
+++ Module15/06_running_nums/main.py
@@ -1,12 +1,13 @@
 number_elements: int = int(input("Введите количество элементов списка: "))
 num_list: list[int] = []
 for _ in range(number_elements):
-    num: int
+    num: int = int(input("Введите число: "))
     num_list.append(num)
 step: int = int(input("Введите шаг сдвига: "))
 new_num_list: int = []
-index: int = -step
-while index != len(num_list) - step:
-    new_num_list.append(num_list[index])
-    index += 1
-print(new_num_list)
+key_count: int = 0
+while key_count < len(num_list):
+    key_count += 1
+    new_num_list.append(num_list[-(step % len(num_list))])
+    step -= 1
+print(f"Сдвинутый список: {new_num_list}")
