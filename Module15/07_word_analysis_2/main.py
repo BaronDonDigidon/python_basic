b089a18f8df60101b43e971616a05029dfc7284d
Gevork <meletjan.kevr94@yandex.ru>
2024-11-20 16:19:07 +0300

    Module 15 add task 7, 8, 9

--- Module15/07_word_analysis_2/main.py
+++ Module15/07_word_analysis_2/main.py
@@ -1,1 +1,9 @@
-# TODO здесь писать код
+palindrome: str = input("Введите слово: ")
+palindrome_symbal_list: list[str] = [sym for sym in palindrome]
+revers_palindrome_list: list [str] = []
+for index in range(-1, -len(palindrome_symbal_list) - 1, -1):
+    revers_palindrome_list.append(palindrome_symbal_list[index])
+if revers_palindrome_list == palindrome_symbal_list:
+    print(f"Слово является палиндромом.")
+else:
+    print(f"Слово не является палиндромом.")
--- Module15/08_sort/main.py
+++ Module15/08_sort/main.py
@@ -1,1 +1,12 @@
-# TODO здесь писать код
+number_elements: int = int(input("Введите количество элементов списка: "))
+num_list: list[int] = []
+for _ in range(number_elements):
+    num: int = int(input("Введите число: "))
+    num_list.append(num)
+for index_num_1 in range(len(num_list)):
+    for index_num_2 in range(len(num_list)):
+        if num_list[index_num_1] < num_list[index_num_2]:
+            save_num: int = num_list[index_num_2]
+            num_list[index_num_2] = num_list[index_num_1]
+            num_list[index_num_1] = save_num
+print(f"Отсортированный список: {num_list}")
