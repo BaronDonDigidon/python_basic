478a63a8f1287fefdff5e98e0b806f3d1b89474a
Gevork <meletjan.kevr94@yandex.ru>
2024-11-25 17:43:19 +0300

    doctring adding v.1.2

--- Module15/07_word_analysis_2/main.py
+++ Module15/07_word_analysis_2/main.py
@@ -1,9 +1,25 @@
-palindrome: str = input("Введите слово: ")
-palindrome_symbal_list: list[str] = [sym for sym in palindrome]
-revers_palindrome_list: list [str] = []
-for index in range(-1, -len(palindrome_symbal_list) - 1, -1):
-    revers_palindrome_list.append(palindrome_symbal_list[index])
-if revers_palindrome_list == palindrome_symbal_list:
-    print(f"Слово является палиндромом.")
-else:
-    print(f"Слово не является палиндромом.")
+from typing import NoReturn
+
+
+def palindrome_compares(palindrome_symbal_list) -> list[str]:
+    revers_palindrome_list: list[str] = []
+    for index in range(-1, -len(palindrome_symbal_list) - 1, -1):
+        revers_palindrome_list.append(palindrome_symbal_list[index])
+    return revers_palindrome_list
+
+
+
+
+def main() -> NoReturn:
+    palindrome: str = input("Введите слово: ")
+    palindrome_symbal_list: list[str] = [sym for sym in palindrome]
+    palindrome_symbal_list_revers = palindrome_compares(palindrome_symbal_list)
+    if palindrome_symbal_list == palindrome_symbal_list_revers:
+        print(f"Слово является палиндромом.")
+    else:
+        print(f"Слово не является палиндромом.")
+
+
+
+
+main()
