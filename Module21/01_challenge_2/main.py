7094dbc92ce3d12f42a58d5b1a432672ae0e608b
Kevork <barondondigidonF@gmail.com>
2025-03-13 02:18:47 +0300

    Module 21 add all task

--- Module21/01_challenge_2/main.py
+++ Module21/01_challenge_2/main.py
@@ -1,1 +1,15 @@
-# TODO здесь писать код
+def num_printer(num: int) -> int:
+    """
+    Принимает число num и выводит числа от 1 до num каждое на отдельной строке.
+    :param num: Число введеное пользователем
+    :return: Выводит числа от 1 до num каддое на отдельной строке
+    """
+    if num > 1:
+        num_printer(num - 1)
+    print(num)
+
+
+
+
+number: int = int(input("Введите число: "))
+num_printer(number)
