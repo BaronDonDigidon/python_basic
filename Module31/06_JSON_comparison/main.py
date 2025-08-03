efce1fdbcd82c575383b29549c18147ca272d0fb
BaronDonDigidon <meletjan.kevr94@yandex.ru>
2025-08-03 20:27:53 +0300

    Update main.py

--- Module31/06_JSON_comparison/main.py
+++ Module31/06_JSON_comparison/main.py
@@ -1,1 +1,9 @@
-# TODO здесь писать код
+from JSONComparator import JSONComparator
+
+if __name__ == "__main__":
+    diff_keys = ["services", "staff", "datetime"]  # список ключей для отслеживания
+
+    comparator = JSONComparator("json_old.json", "json_new.json", diff_keys)
+    result = comparator.compare()
+    print(result)
+    comparator.save_result()
